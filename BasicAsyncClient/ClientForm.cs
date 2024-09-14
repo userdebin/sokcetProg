3cd1aef6a20d44810b60f26b451b06087d58eb8b
devina yusra <145321227+userdebin@users.noreply.github.com>
2024-09-14 16:41:57 +0700

    Add files via upload

---
+++ BasicAsyncClient/ClientForm.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using MovingObjectClient;
+
+namespace BasicAsyncClient
+{
+    public partial class ClientForm : Form
+    {
+        private Socket clientSocket;
+        private byte[] buffer;
+        private byte[] buffer2;
+        private MOClientForm1 _moClientForm1;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+            _moClientForm1 = new MOClientForm1();
+            _moClientForm1.Show();
+        }
+
+        private static void ShowErrorDialog(string message)
+        {
+            MessageBox.Show(message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            try
+            {
+                int received = clientSocket.EndReceive(AR);
+
+                if (received == 0)
+                {
+                    return;
+                }
+
+
+                string message = Encoding.ASCII.GetString(buffer, 0 , received);
+                if (TryParseRectangle(message, out Rectangle rect))
+                {
+                    _moClientForm1?.UpdateRectangle(rect);
+                }
+                Invoke((Action)delegate { Text = "Server says: " + message; });
+                Console.WriteLine("Server says: " + message);
+                // Start receiving data again.
+                clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, null);
+            }
+            // Avoid Pokemon exception handling in cases like these.
+            catch (SocketException ex)
+            {
+                ShowErrorDialog(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                ShowErrorDialog(ex.Message);
+            }
+        }
+
+        private void ConnectCallback(IAsyncResult AR)
+        {
+            try
+            {
+                clientSocket.EndConnect(AR);
+                UpdateControlStates(true);
+                buffer = new byte[clientSocket.ReceiveBufferSize];
+                clientSocket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ReceiveCallback, null);
+            }
+            catch (SocketException ex)
+            {
+                ShowErrorDialog(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                ShowErrorDialog(ex.Message);
+            }
+        }
+
+        private void SendCallback(IAsyncResult AR)
+        {
+            try
+            {
+                clientSocket.EndSend(AR);
+            }
+            catch (SocketException ex)
+            {
+                ShowErrorDialog(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                ShowErrorDialog(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// A thread safe way to enable the send button.
+        /// </summary>
+        private void UpdateControlStates(bool toggle)
+        {
+            Invoke((Action)delegate
+            {
+                buttonSend.Enabled = toggle;
+                buttonConnect.Enabled = !toggle;
+                labelIP.Visible = textBoxAddress.Visible = !toggle;
+            });
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Serialize the textBoxes text before sending.
+                PersonPackage person = new PersonPackage(checkBoxMale.Checked, (ushort)numberBoxAge.Value,
+                    textBoxEmployee.Text);
+                byte[] buffer = person.ToByteArray();
+                clientSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, SendCallback, null);
+            }
+            catch (SocketException ex)
+            {
+                ShowErrorDialog(ex.Message);
+                UpdateControlStates(false);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                ShowErrorDialog(ex.Message);
+                UpdateControlStates(false);
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // Connect to the specified host.
+                var endPoint = new IPEndPoint(IPAddress.Parse(textBoxAddress.Text), 3333);
+                clientSocket.BeginConnect(endPoint, ConnectCallback, null);
+            }
+            catch (SocketException ex)
+            {
+                ShowErrorDialog(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                ShowErrorDialog(ex.Message);
+            }
+        }
+
+
+// Method to parse the Rectangle.ToString() format
+        private bool TryParseRectangle(string input, out Rectangle rectangle)
+        {
+            rectangle = new Rectangle();
+
+            try
+            {
+                // Remove the surrounding braces and split by commas
+                // Expected format: {X=20,Y=20,Width=30,Height=30}
+                string[] parts = input.Trim('{', '}').Split(',');
+
+                // Create a dictionary to store the parsed values
+                Dictionary<string, int> values = new Dictionary<string, int>();
+
+                foreach (string part in parts)
+                {
+                    // Split each part by '='
+                    string[] pair = part.Split('=');
+                    if (pair.Length == 2 && int.TryParse(pair[1], out int value))
+                    {
+                        values[pair[0].Trim()] = value;
+                    }
+                }
+
+                // Check if all required parts are present
+                if (values.ContainsKey("X") && values.ContainsKey("Y") &&
+                    values.ContainsKey("Width") && values.ContainsKey("Height"))
+                {
+                    // Construct the rectangle using parsed values
+                    rectangle = new Rectangle(
+                        values["X"],
+                        values["Y"],
+                        values["Width"],
+                        values["Height"]
+                    );
+                    return true;
+                }
+            }
+            catch
+            {
+                // Parsing failed
+            }
+
+            return false;
+        }
+    }
+}
---
+++ BasicAsyncServer/ServerForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace BasicAsyncServer
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.clmName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmAge = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmIsMale = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.clmName, this.clmAge, this.clmIsMale });
+            this.dataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView.Location = new System.Drawing.Point(18, 18);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.Size = new System.Drawing.Size(488, 635);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // clmName
+            // 
+            this.clmName.HeaderText = "Name";
+            this.clmName.Name = "clmName";
+            // 
+            // clmAge
+            // 
+            this.clmAge.HeaderText = "Age";
+            this.clmAge.Name = "clmAge";
+            // 
+            // clmIsMale
+            // 
+            this.clmIsMale.HeaderText = "Male";
+            this.clmIsMale.Name = "clmIsMale";
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 672);
+            this.Controls.Add(this.dataGridView);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ServerForm";
+            this.Text = "Basic Async Server";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmAge;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmIsMale;
+
+    }
+}
+
